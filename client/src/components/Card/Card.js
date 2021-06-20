81fe326a21e241b18393637aa5ea255fb7e388bd
NatashaSilva <natasha_ssn@hotmail.com>
2021-06-19 22:17:56 -0400

    Refactoring user page and creating card component

---
+++ client/src/components/Card/Card.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+const Card = ({ user }) => {
+  const [showSkills, setShowSkills] = useState(false);
+  const [showInterest, setShowInterest] = useState(false);
+
+  const handleSkillsClick = (event) => {
+    event.preventDefault();
+    setShowSkills(!showSkills);
+  };
+
+  const handleInterestClick = (event) => {
+    event.preventDefault();
+    setShowInterest(!showInterest);
+  };
+
+  const hasInterests = (interests) => interests?.length > 0;
+
+  const hasSkills = (skills) => skills?.length > 0;
+
+  if (!user) return null;
+
+  return (
+    <div key={user.id} className="user-card__info">
+      <div className="user-card__info--title">
+        <img className="user-card__info--img" alt=""></img>
+        <h2 className="user-card__info--name">{user.name}</h2>
+      </div>
+      <div className="user-card__info--bio">
+        <p>{user.bio}</p>
+      </div>
+      <div className="user-card__info--option">
+        <div className="user-card__info--skills">
+          <button
+            to="/skills"
+            className="user-card__info--button"
+            onClick={handleSkillsClick}
+          >
+            Skills
+          </button>
+          <ul
+            className={
+              showSkills ? "user-card__info--list" : "user-card__info--hidden"
+            }
+          >
+            {hasSkills(user.skills) ? (
+              user.skills?.map((skill) => <li key={skill.id}>{skill}</li>)
+            ) : (
+              <p>This user has no skills.</p>
+            )}
+          </ul>
+        </div>
+        <div className="user-card__info--interest">
+          <button
+            to="/interest"
+            className="user-card__info--button"
+            onClick={handleInterestClick}
+          >
+            Interest
+          </button>
+          <ul
+            className={
+              showInterest ? "user-card__info--list" : "user-card__info--hidden"
+            }
+          >
+            {hasInterests(user.interests) ? (
+              user.interests?.map((interest) => (
+                <li key={interest.id}>{interest}</li>
+              ))
+            ) : (
+              <p>This user has no interests.</p>
+            )}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import axios from "axios";
 import { API_URL_ROOT, API_USERS_PATH } from "../apiLink.js";
+import Card from "../../components/Card/Card";
 
 const User = () => {
   const [showSkills, setShowSkills] = useState(false);
@@ -41,16 +42,6 @@ const User = () => {
       });
   }, []);
 
-  const handleSkillsClick = (event) => {
-    event.preventDefault();
-    setShowSkills(!showSkills);
-  };
-
-  const handleInterestClick = (event) => {
-    event.preventDefault();
-    setShowInterest(!showInterest);
-  };
-
   if (!users) {
     return <p>No users yet.</p>;
   }
@@ -62,57 +53,7 @@ const User = () => {
       <Header user={user} />
       <div className="user-card">
         {users.map((user) => (
-          <div key={user.id} className="user-card__info">
-            <div className="user-card__info--title">
-              <img className="user-card__info--img" alt=""></img>
-              <h2 className="user-card__info--name">{user.name}</h2>
-            </div>
-            <div className="user-card__info--bio">
-              <p>{user.bio}</p>
-            </div>
-            <div className="user-card__info--option">
-              <div className="user-card__info--skills">
-                <button
-                  to="/skills"
-                  className="user-card__info--button"
-                  onClick={handleSkillsClick}
-                >
-                  Skills
-                </button>
-                <ul
-                  className={
-                    showSkills
-                      ? "user-card__info--list"
-                      : "user-card__info--hidden"
-                  }
-                >
-                  {user.skills?.map((skill) => (
-                    <li key={skill.id}>{skill}</li>
-                  ))}
-                </ul>
-              </div>
-              <div className="user-card__info--interest">
-                <button
-                  to="/interest"
-                  className="user-card__info--button"
-                  onClick={handleInterestClick}
-                >
-                  Interest
-                </button>
-                <ul
-                  className={
-                    showInterest
-                      ? "user-card__info--list"
-                      : "user-card__info--hidden"
-                  }
-                >
-                  {user.interest?.map((interest) => (
-                    <li key={interest.id}>{interest}</li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
+          <Card user={user} />
         ))}
       </div>
     </div>

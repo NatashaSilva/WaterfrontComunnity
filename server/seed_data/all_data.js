2ce803a4dc2e36051da9d6c59b70c6034f2bc9cb
NatashaSilva <natasha_ssn@hotmail.com>
2021-06-22 21:38:49 -0400

    register users

--- server/seed_data/all_data.js
+++ server/seed_data/all_data.js
@@ -1,45 +1,57 @@
-[{
-    name: 'Andrea Andrade',
-    birth: '02/10/1993',
-    bio: 'I love puzzles and solving problems. Currently working on improving my painting skills.',
-    skills: ['Software Engineer'],
-    interest: ['Learning Languages', 'Painting'],
-    email: 'carolinacabral@gmail.com',
-    password: '123'
-},
-{
-    name: 'Carolina Cabral',
-    birth: '04/17/1990',
-    bio: 'Love talking about politics and music. My dream is to become a musician.',
-    skills: ['Musician', 'Software Engineer'],
-    interest: ['Music'],
-    email: 'carolinacabral@gmail.com',
-    password: '123'
-},
-{
-    name: 'Manoela Brito',
-    birth: '08/17/1988',
-    bio: 'I love to spend time in the nature, camping, reading, and meeting new people.',
-    skills: ['Basic Furniture Assembler'],
-    interest: ['Outdoor Activities', 'Camping', 'Traveling','Indoor Activities'],
-    email: 'manoelabrito@gmail.com',
-    password: '123'
-},
-{
-    name: 'Marina Abreu',
-    birth: '05/24/1991',
-    bio: 'Exploring the world and myself. I love to spend time outside chatting with friends',
-    skills: ['Babysitter', 'Basic Cooking'],
-    interest: ['Movies', 'Hiking', 'Traveling'],
-    email: 'marinaabreu@gmail.com',
-    password: '123'
-},
-{
-    name: 'Natasha da Silva',
-    birth: '08/21/1991',
-    bio: 'Just moved to Toronto, love to spend time in the nature and learning new things.',
-    skills: ['Basic photographer'],
-    interest: ['Movies', 'Camping', 'Traveling', 'Yoga', 'Cycling'],
-    email: 'natashasilva@gmail.com',
-    password: '123'
-}]    
+[
+  {
+    name: "Andrea Andrade",
+    birth: "02/10/1993",
+    bio:
+      "I love puzzles and solving problems. Currently working on improving my painting skills.",
+    skills: ["Software Engineer"],
+    interest: ["Learning Languages", "Painting"],
+    email: "carolinacabral@gmail.com",
+    password: "123",
+  },
+  {
+    name: "Carolina Cabral",
+    birth: "04/17/1990",
+    bio:
+      "Love talking about politics and music. My dream is to become a musician.",
+    skills: ["Musician", "Software Engineer"],
+    interest: ["Music"],
+    email: "carolinacabral@gmail.com",
+    password: "123",
+  },
+  {
+    name: "Manoela Brito",
+    birth: "08/17/1988",
+    bio:
+      "I am an environmentalist. Also love spending time in nature, camping, reading, and meeting new people.",
+    skills: ["Basic Furniture Assembler"],
+    interest: [
+      "Outdoor Activities",
+      "Camping",
+      "Traveling",
+      "Indoor Activities",
+    ],
+    email: "manoelabrito@gmail.com",
+    password: "123",
+  },
+  {
+    name: "Marina Abreu",
+    birth: "05/24/1991",
+    bio:
+      "A true explorer. I love to spend time outside chatting with friends and discovering new places in my neighborhood.",
+    skills: ["Babysitter", "Basic Cooking"],
+    interest: ["Movies", "Hiking", "Traveling"],
+    email: "marinaabreu@gmail.com",
+    password: "123",
+  },
+  {
+    name: "Natasha da Silva",
+    birth: "08/21/1991",
+    bio:
+      "Passionate about learning new things and traveling. I  love spending time in nature, reading books about politics and the environment, and chatting with friends.  ",
+    skills: ["Basic photographer"],
+    interest: ["Movies", "Camping", "Traveling", "Yoga", "Cycling"],
+    email: "natashasilva@gmail.com",
+    password: "123",
+  },
+];
--- server/seeds/users_skills.js
+++ server/seeds/users_skills.js
@@ -1,37 +1,16 @@
-const skillsData = require('../seed_data/skills');
-const usersData = require('../seed_data/users');
-const interestData = require('../seed_data/interest');
-const usersSkillsData = require('../seed_data/users_skills');
-const usersInterestData = require('../seed_data/users_interest');
+const skillsData = require("../seed_data/skills");
+const interestData = require("../seed_data/interest");
 
 exports.seed = function (knex) {
-  return knex('users')
+  return knex("skills")
     .del()
     .then(function () {
-      return knex('users').insert(usersData);
+      return knex("skills").insert(skillsData);
     })
     .then(function () {
-      return knex('skills').del();
+      return knex("interest").del();
     })
     .then(function () {
-      return knex('skills').insert(skillsData);
-    })
-    .then(function () {
-        return knex('users_skills').del();
-      })
-      .then(function () {
-        return knex('users_skills').insert(usersSkillsData);
-    })
-    .then(function () {
-        return knex('interest').del();
-    })
-    .then(function () {
-    return knex('interest').insert(interestData);
-    })
-    .then(function () {
-        return knex('users_interest').del();
-    })
-    .then(function () {
-        return knex('users_interest').insert(usersInterestData);
+      return knex("interest").insert(interestData);
     });
 };
